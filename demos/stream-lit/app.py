32f9011f60bde45134437eda7c47fbfac8390a6e
CaoBaoQi <caobq1024@gmail.com>
2024-10-12 11:27:28 +0800

    :sparkles: feat: streamlit demo

---
+++ demos/stream-lit/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
+
+st.title("波兰球 —— 高山下的花环")
+st.markdown("[![Click me](app/static/poland_ball.jpg)](https://streamlit.io)")
